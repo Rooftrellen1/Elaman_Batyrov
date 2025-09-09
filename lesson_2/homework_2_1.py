d80675d7344e001f0ad1a98700a4e90b65bc3586
region_026 <tutkabaev777@gmail.com>
2025-09-09 19:03:19 +0500

    Homework 2

---
+++ lesson_2/homework_2_1.py
@@ -0,0 +1,55 @@
+# 1
+a = 8
+b = 3
+x = 2.5
+y = -1.7
+print(type(a),type(b),type(x),type(y))
+
+# 2
+
+num1 = 50
+num2 = 25
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1 ** num2)
+
+# 3
+
+a = 10
+b = 3
+print(a / b)
+print(a // b)
+print(a % b)
+a = -10
+b = 3
+print(a / b)
+print(a // b)
+print(a % b)
+a = 10
+b = -3
+print(a / b)
+print(a // b)
+print(a % b)
+
+# 4
+
+print(5 + 3 * 2)
+print((5 + 3) * 2)
+print(10 / 2**2)
+
+# 5
+
+count = 10
+count += 5
+count -= 3
+count *= 2
+count /= 4
+print(count)
+
+
+
+
